5990ff7b2bcf75852c618a9619ab372e81f797b7
DevanshShukla1 <78690713+DevanshShukla1@users.noreply.github.com>
2022-10-02 20:36:56 +0530

    Update script.js

--- Module4solution/script.js
+++ Module4solution/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Ram";
-names[1]="Jasmine";
-names[2]="Jenny";
-names[3]="jalaj";
-names[4]="passepartout";
-names[5]="franky";
-names[6]="lamar";
-names[7]="pearson";
-names[8]="linlin";
-names[9]="jasper";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
